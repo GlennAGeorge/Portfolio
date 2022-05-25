95392d6c690eb1d5e4f00d82bf5ceefb3d6a7d07
Glenn George <ggpersonaltraining@gmail.com>
2022-05-25 23:28:31 +1000

    loader fixes, complete

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-//Get the button:
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("contents").style.display = "block";
+}
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -11,9 +18,8 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
+
